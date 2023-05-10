33c8fb649f64219f034eaefa2f8e540693714556
Your Name <you@example.com>
2023-05-10 21:02:38 +0200

    blogs react done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NotFound from "./NotFound";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/create" element={<Create />} />
+            <Route exact path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/BlogDetails.js
@@ -0,0 +1,37 @@
+import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+import { useNavigate } from "react-router-dom";
+
+const BlogDetails = () => {
+  const { id } = useParams();
+  const {
+    error,
+    isPending,
+    data: blog,
+  } = useFetch("http://localhost:8000/blogs/" + id);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      navigate("/");
+    });
+  };
+  return (
+    <div className="blog-details">
+      {isPending && <div> Loading ... </div>}
+      {error && <div> {error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div> {blog.body}</div>
+          <button onClick={handleClick}>Delete blog</button>
+        </article>
+      )}
+    </div>
+  );
+};
+
+export default BlogDetails;
